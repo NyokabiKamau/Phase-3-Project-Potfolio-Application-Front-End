ab23a801920a652ee3b85157885284d7ef4486cd
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-03 12:45:28 +0300

    installed react-router and react-router dom dependancies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React, {BrowserRoute} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRoute>
+    <BrowserRouter>
       <App />
-    </BrowserRoute>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
