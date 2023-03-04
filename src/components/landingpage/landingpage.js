5dbffa2842b328ac3cdef3dadf3e2ef880c19ea4
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-04 14:27:05 +0300

    created a landing page template

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React from "react"
+import './landingpage.css'
+import image from "./images/logo.png"
+import image2 from "./images/photo.jpg"
 
-function LandingPage() {
-    return (
-        <div></div>
+function Landingpage(){
+
+    return(
+        <div className="header">
+            <div className="top-nav">
+                <ul>
+                    <li><a>HOME</a></li>
+                    <li><a>OUR WORK</a></li>
+                    <li><a>WHAT WE DO</a></li>
+                    <li><a>ABOUT US</a></li>
+                </ul>
+                <button>Contact Us</button>
+            </div>
+
+            <div className="side-nav">
+                <img src={image} className="logo" />
+                <a>LATEST PROJECTS</a>
+            </div>
+            <div className="text-box">
+                <p>WE HELP YOU TO</p>
+                <h2>Track Your Projects & Skills</h2>
+                <button id="get">Your Journey Begins Here</button>
+            </div>
+            {/* <img id="image" src={image2}/> */}
+        </div>
     )
 }
 
-export default LandingPage
+
+export default Landingpage;
