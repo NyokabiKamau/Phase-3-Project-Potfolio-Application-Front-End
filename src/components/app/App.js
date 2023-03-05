6f0672590dad277848b7ba3c5afbc706f103338f
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-05 11:47:13 +0300

    added routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,19 +4,29 @@ import { Routes, Route} from 'react-router-dom';
 import Home from "../home/home";
 import Project from "../project/project";
 import Header from '../navbar/header';
+// import Landingpage from '../landingpage/landingpage';
+import Login from '../login/login';
+import CreateProject from '../project/create-project';
+import Skill from '../skills/skills-data/skills';
+
+
 function App() {
   return (
-    <div>
+    <>
+      {/* <Landingpage/> */}
+      {/* <Login/> */}
       <Header/>
-      <div className='mt-5'>
-      </div>
       <main>
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/projects" element={<Project />} />
+          <Route path="/skills" element={<Skill />} />
+          <Route path="/auth/reg" element={<Login />} />
+          <Route path="/projects/create" element={<CreateProject/>}/>
         </Routes>
       </main>
-    </div>
+     
+    </>
   )
 }
 
